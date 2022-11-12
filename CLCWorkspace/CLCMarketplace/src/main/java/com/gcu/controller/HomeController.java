fa244b3269720198b81aa629e19e6e1de8965f0d
Putrance <aaron.rawley@hotmail.com>
2022-11-12 13:46:16 -0700

    Updating inline comments

--- CLCWorkspace/CLCMarketplace/src/main/java/com/gcu/controller/HomeController.java
+++ CLCWorkspace/CLCMarketplace/src/main/java/com/gcu/controller/HomeController.java
@@ -11,13 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gcu.model.ShirtModel;
 
+//Homepage controller
 @Controller
 @RequestMapping("/")
 public class HomeController {
 	@GetMapping("/")
 	public String printHome(Model model)
 	{
-		// Create some shirts
+		// Create some shirts to populate table on homepage (To be updated with DB Query)
 		List<ShirtModel> shirts = new ArrayList<ShirtModel>();
 		shirts.add(new ShirtModel("blue", "small", 2));
 		shirts.add(new ShirtModel("green", "medium", 3));
@@ -25,7 +26,7 @@ public class HomeController {
 		shirts.add(new ShirtModel("red", "x-small", 6));
 		shirts.add(new ShirtModel("purple", "large", 10));
 		
-		
+		// Return models on home.html homepage view
 		model.addAttribute("title", "Welcome to the CLC Marketplace!");
 		model.addAttribute("shirts", shirts);
 		return "home";
