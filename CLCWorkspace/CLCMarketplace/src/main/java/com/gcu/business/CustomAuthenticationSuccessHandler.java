58778ace64d82c90908c2242ddc75595528a4de0
Putrance <aaron.rawley@hotmail.com>
2022-11-20 01:44:58 -0700

    Updated login module to use Springbeans, SpringFramework Security, and inMemoryAuth

---
+++ CLCWorkspace/CLCMarketplace/src/main/java/com/gcu/business/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,22 @@
+package com.gcu.business;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException 
+	{
+		System.out.println("Logged user: " + authentication.getName());
+
+	    response.sendRedirect("/");
+	}
+
+}
